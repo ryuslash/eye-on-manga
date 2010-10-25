10ac8429513186e5e396167dcdb7459fa3932525
Tom Willemsen <ryuslash@gmail.com>
2010-10-25 23:18:15 +0200

    Makes
    using automake it can be built and installed

--- antigen.sh
+++ antigen.sh
@@ -6,4 +6,4 @@ fi
 
 rm -f *.tar.* *.tgz
 rm -Rf autom4te.cache
-rm -f src/Makefile.in Makefile.in aclocal.m4 configure depcomp install-sh missing build-stamp configure-stamp
+rm -f src/Makefile.in data/Makefile.in Makefile.in aclocal.m4 configure depcomp install-sh missing build-stamp configure-stamp
